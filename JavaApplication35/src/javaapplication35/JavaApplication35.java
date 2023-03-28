4bfdb6b80c5066e07c2e4a6ba3048431d0fcdabb
tran tri <tranvantri352@gmail.com>
2023-03-28 16:53:47 +0700

    Phep Tru

---
+++ JavaApplication35/src/javaapplication35/JavaApplication35.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication35;
+
+/**
+ *
+ * @author NGUYEN NGOC TRUONG
+ */
+public class JavaApplication35 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public double tru(String A,String B){
+        return Double.parseDouble(A) - Double.parseDouble(B);
+    }
+   
+}
