ce3bda4e2c8db03d5d0e01d3a3b2321e5f9b0eba
ngoctruong180602 <ngoctruong180603.com>
2023-04-01 14:44:36 +0700

    checkin by Ngoc Truong

--- JavaApplication24/src/javaapplication24/information.java
+++ JavaApplication24/src/javaapplication24/information.java
@@ -429,6 +429,7 @@ public class information extends javax.swing.JFrame {
             java.util.logging.Logger.getLogger(information.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
+        //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
